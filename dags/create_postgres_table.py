033b1210988d519b4ff40dc556bdd5eb6a061436
Rohith131102 <rohith.reddy@sigmoidanalytics.com>
2023-06-22 15:01:01 +0530

    added comments to dag.py

--- dags/create_postgres_table.py
+++ dags/create_postgres_table.py
@@ -2,9 +2,10 @@ from airflow import DAG
 from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 
-with DAG(dag_id='time_task', start_date=datetime(2022, 1, 1), 
-        schedule_interval='@daily',  catchup=False) as dag:
+# Create a DAG with the specified DAG ID, start date, and schedule interval
+with DAG(dag_id='time_task', start_date=datetime(2022, 1, 1), schedule_interval='@daily', catchup=False) as dag:
 
+    # Task to create the table if it doesn't exist
     create_table = PostgresOperator(
         task_id='create_table',
         postgres_conn_id='postgres',
@@ -15,13 +16,16 @@ with DAG(dag_id='time_task', start_date=datetime(2022, 1, 1),
         '''
     )
     
+    # Task to insert a value into the table with the current timestamp
     insert_value = PostgresOperator(
-        task_id = 'insert_value',
+        task_id='insert_value',
         postgres_conn_id='postgres',
-        sql = '''
-                INSERT INTO myTime_assignment VALUES (CURRENT_TIMESTAMP);   
+        sql='''
+            INSERT INTO myTime_assignment VALUES (CURRENT_TIMESTAMP);   
         '''
     )
     
+    # Set task dependencies
     create_table >> insert_value
+
     
